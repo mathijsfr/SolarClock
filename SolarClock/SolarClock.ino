f27a893957d8bc8e0cc87932f5e0b155871c9a5f
Unknown <bram.lemmen@gmail.com>
2017-11-10 14:37:41 +0100

    CommunicationHandler added
    Added CommunicationHandler.cpp file and implementation for constructor. Added and implemented ConnectToServer function.

--- SolarClock/SolarClock.ino
+++ SolarClock/SolarClock.ino
@@ -1,17 +1,18 @@
 #include "SolarClock.h"
-#include "Datahandler.h"
+#include "DataHandler.h"
 
 #define MotorCount 12
 
 int motorPins[MotorCount] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
-DataHandler* dataHandler = new DataHandler();
-BarHandler* barhandler = new BarHandler(motorPins, *dataHandler);
+DataHandler dataHandler = new DataHandler();
+BarHandler barhandler = new BarHandler(motorPins, &dataHandler);
 
-CommunicationHandler* communicationHandler = new CommunicationHandler();
-WatchDogTimer* watchDogTimer = new WatchDogTimer();
+CommunicationHandler communicationHandler = new CommunicationHandler();
+WatchDogTimer watchDogTimer = new WatchDogTimer();
 
-SolarClock* solarClock = new SolarClock(*barhandler, *communicationHandler, *watchDogTimer);
+solarClock = new SolarClock(&barhandler, &communicationHandler, &watchDogTimer);
 
+Events ev;
 
 void setup()
 {
@@ -20,12 +21,6 @@ void setup()
 
 void loop()
 {
-	Events ev = GetEvent();
-
-	solarClock->HandleEvent(ev);
+	solarClock.HandleEvent(ev);
 }
 
-Events GetEvent()
-{
-	return NO_EVENT;
-}
